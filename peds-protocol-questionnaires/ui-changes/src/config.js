4fb0ce931bcd3f2bb64d4964c33924e0735f9e5d
Yibei Chen <yibeichan@gmail.com>
2025-08-01 10:55:35 -0400

    Update config.js files to point to upstream repository
    - Changed GitHub URLs from yibeichan/b2ai-peds-protocol to kind-lab/b2ai-peds-protocol
    - Updated commit hash to 7b18a24 for both protocols
    - Prepares repository for pull request to upstream
    
    This ensures the deployed protocols will reference the canonical upstream
    repository instead of the fork when the PR is merged.

--- peds-protocol-questionnaires/ui-changes/src/config.js
+++ peds-protocol-questionnaires/ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     /* eslint-disable */
-    githubSrc: 'https://raw.githubusercontent.com/yibeichan/b2ai-peds-protocol/299c293/peds-protocol-questionnaires/peds-protocol/peds-protocol',
+    githubSrc: 'https://raw.githubusercontent.com/kind-lab/b2ai-peds-protocol/7b18a24/peds-protocol-questionnaires/peds-protocol/peds-protocol',
     banner: 'B2AI data collection',
     startButton: 'Start',
     assetsPublicPath: '/b2ai-peds-protocol/questionnaires/',
