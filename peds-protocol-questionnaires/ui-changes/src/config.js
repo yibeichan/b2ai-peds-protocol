eef06465546c610eca2b39e60948aae06495adb3
Yibei Chen <yibeichan@gmail.com>
2025-07-31 21:34:58 -0400

    Add dual protocol deployment workflow
    - Added GitHub workflow for building both peds-protocol-combined and peds-protocol-questionnaires
    - Creates landing page with links to both protocol UIs
    - Uses stable reproschema-ui commit hash for reproducible builds
    - Deploys to GitHub Pages with separate URLs for each protocol

---
+++ peds-protocol-questionnaires/ui-changes/src/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    /* eslint-disable */
+    githubSrc: 'https://github.com/yibeichan/b2ai-peds-protocol/blob/9b2b20498ca242a0e19301cd873d06c5a9068d2f/peds-protocol-questionnaires/peds-protocol/peds-protocol.json',
+    banner: 'B2AI data collection',
+    startButton: 'Start',
+    assetsPublicPath: '/b2ai-peds-protocol-questionnaires/',
+    backendServer: null
+  };
